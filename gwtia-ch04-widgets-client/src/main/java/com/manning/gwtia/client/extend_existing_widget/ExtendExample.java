a7216a847fd775a2d54964c3567bdd53e5121f0e
DaLei Sun <sundalei1988@gmail.com>
2019-01-28 16:49:08 +0800

    test

--- gwtia-ch04-widgets-client/src/main/java/com/manning/gwtia/client/extend_existing_widget/ExtendExample.java
+++ gwtia-ch04-widgets-client/src/main/java/com/manning/gwtia/client/extend_existing_widget/ExtendExample.java
@@ -1,20 +1,10 @@
-/**
- * 
- */
 package com.manning.gwtia.client.extend_existing_widget;
 
-// The rest of these imports just relate to standard user interface aspects.
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 
 /**
  * This example shows how to create a widget directly from the DOM.
- * 
- * The example builds a simple canvas widget (although GWT 2.2. introduces a specific GWT
- * widget, we keep this example as it shows how to build a widget)
- * 
  */
 public class ExtendExample extends Composite{
 
@@ -32,14 +22,7 @@ public class ExtendExample extends Composite{
 	public ExtendExample(){
 		theLabel = new ReportSizeLabel("What's my size?");
 		initWidget(theLabel);
-		
-		// Add a ClickHandler to the label - this should cause an exception to be raised
-		// since ReportSizeLabel overides the addClickHandler method of InnerLabel to explicitly
-		// prevent a ClickHandler being added
-		theLabel.addClickHandler(new ClickHandler(){
-			public void onClick(ClickEvent event) {
-				Window.alert("You clicked me!");
-			}			
-		});
+
+		theLabel.addClickHandler(event -> Window.alert("You clicked me!"));
 	}
 }
--- gwtia-ch04-widgets-client/src/main/java/com/manning/gwtia/client/extend_existing_widget/ReportSizeLabel.java
+++ gwtia-ch04-widgets-client/src/main/java/com/manning/gwtia/client/extend_existing_widget/ReportSizeLabel.java
@@ -6,30 +6,30 @@ import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.InlineLabel;
 
-public class ReportSizeLabel extends InlineLabel{
+public class ReportSizeLabel extends InlineLabel {
 
-	/**
-	 * Simple constructor calling the parent constructor
-	 */
-	public ReportSizeLabel(String txt){
-		super(txt);
-	}
-	
-	/**
-	 * Override the parent's addClickHandler to register an exception that click handlers
-	 * cannot be added to this widget.
-	 */
-    public HandlerRegistration addClickHandler(ClickHandler handler){ 
-        GWT.log("", new Exception("Cannot add ClickHandler to ReportSizeLabel"));                      
-        return null;                                                  
-     }
-	
+    /**
+     * Simple constructor calling the parent constructor
+     */
+    public ReportSizeLabel(String txt) {
+        super(txt);
+    }
+
+    /**
+     * Override the parent's addClickHandler to register an exception that click handlers
+     * cannot be added to this widget.
+     */
+    public HandlerRegistration addClickHandler(ClickHandler handler) {
+        GWT.log("", new Exception("Cannot add ClickHandler to ReportSizeLabel"));
+        return null;
+    }
+
     /**
      * Override the parent's onLoad method to display the dimensions.
      * We use the onLoad method as this is called by the GWT widget attach mechanism
      * only once the widget is attached in the DOM (thus it will have dimensions).
      */
-	public void onLoad(){
-		Window.alert("Dimensions: "+this.getOffsetWidth()+" x " + this.getOffsetHeight());
-	}
+    public void onLoad() {
+        Window.alert("Dimensions: " + this.getOffsetWidth() + " x " + this.getOffsetHeight());
+    }
 }
